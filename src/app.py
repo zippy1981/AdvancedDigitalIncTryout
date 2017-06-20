9448665ed11e1ad48394b79a86d1eb42790baa33
Justin Dearing <zippy1981@gmail.com>
2017-06-20 14:24:24 -0400

    png uplaoding works. Initial API consumption added

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
 import boto3
-from chalice import Chalice
+from chalice import Chalice, BadRequestError
 import configparser
-import json
+import imghdr
+from ipaddress import ip_address
+from png import png
 import os
+from time import localtime, time
 import uuid
 
 config = configparser.ConfigParser()
@@ -15,6 +18,69 @@ S3 = boto3.client('s3', region_name='us-east-1')
 
 app = Chalice(app_name='src')
 app.debug = True
+
+def get_html_url(html_file):
+    return "https://s3.amazonaws.com/{0}/{1}".format(BUCKET, html_file)
+
+def get_latitude_from_ip(ip_addr):
+    """
+    Using a simple cross multplying algorithm to convert your ipv4 address into a latitude 0.0.0.0 becomes
+    :param ip_addr: Ip address in any form castable by ipaddress.ip_address
+    :return: a longitude between -90 and 90
+    """
+    ip_int = float(int(ip_address(ip_addr)))
+    return 90.0-((180.0*ip_int) / (float(2 ** 32)))
+
+
+def get_get_longitude_from_time(time_stamp = None):
+    """
+    Converts the hours and minues in a timestamp to a latitude.
+    :param time_stamp: Unix timestamp
+    :return: A latitude between -180 and 180
+    """
+    if time_stamp is None:
+        time_stamp = time()
+    time_s = localtime(time_stamp)
+    longitude = (float)(time_s.tm_hour * 15.0)
+    longitude += (float)(time_s.tm_min)/60.0
+    longitude -= 180
+    return longitude
+
+def get_png_dimensions(png_file):
+    # Some kind of splatting could probably avoid the need to set this explicitly
+    reader = png.Reader(bytes=png_file)
+    img = reader.read()
+    return {
+        'length': img[0],
+        'width': img[1]
+    }
+
+def get_png_url(file_name):
+    return "https://s3.amazonaws.com/{0}/{1}".format(BUCKET, file_name)
+
+def generate_template(png_file, dimensions):
+    with open(PNG_TEMPLATE_PATH, 'r') as template:
+        return template.read().format(BUCKET, png_file, dimensions['length'], dimensions['width'])
+
+def scale_dimensions(dimensions, max_dimension):
+    if (dimensions['length'] <= max_dimension and dimensions['width'] <= max_dimension):
+        return dimensions
+    elif (dimensions['length'] == dimensions['width']):
+        return {
+            'length' : max_dimension,
+            'width' : max_dimension,
+        }
+    elif (dimensions['length'] > dimensions['width']):
+        return {
+            'length' : max_dimension,
+            'width' : (dimensions['width'] * max_dimension)/dimensions['length'],
+        }
+    else:
+        return {
+            'width': max_dimension,
+            'length': (dimensions['length'] * max_dimension) / dimensions['width'],
+        }
+
 
 @app.route('/')
 def index():
@@ -30,17 +96,25 @@ def status():
     """
     return 'OK'
 
-
-@app.route('/piper')
-def processFeed():
-    raise NotImplementedError('I need to finish writing this')
+@app.route('/map')
+def process_feed():
+    client_ip = app.current_request.context['identity']['sourceIp']
+    url = "http://staticmap.openstreetmap.de/staticmap.php?center={0},{1}&zoom=14&size=400x200&maptype=mapnik"\
+        .format(get_latitude_from_ip(client_ip), get_get_longitude_from_time())
+    return { 'map_url': url }
 
 @app.route('/png', methods=['POST'], content_types=['image/png'])
-def pushPng():
+@app.route('/png/{max_scale_dimension}', methods=['POST'], content_types=['image/png'])
+def push_png(max_scale_dimension=100):
     request = app.current_request
+    actual_image_type = imghdr.what(None, h=request.raw_body)
+    if actual_image_type != 'png':
+        raise BadRequestError("Type of uploaded image is not png. [ actual = {0}]".format(actual_image_type))
+
     key = uuid.uuid4()
     png_file = "{0}.png".format(key)
     html_file = "{0}.html".format(key)
+
     S3.put_object(
         Bucket=BUCKET,
         Key=png_file,
@@ -49,18 +123,35 @@ def pushPng():
         ContentDisposition='inline',
         ContentType='image/png'
     )
-    with open(PNG_TEMPLATE_PATH, 'r') as template:
-        html = template.read().format(BUCKET, png_file)
-        S3.put_object(
-            Bucket=BUCKET,
-            Key=html_file,
-            Body=html,
-            ACL='public-read',
-            ContentDisposition='inline',
-            ContentType='text/html'
-        )
+
+    dimensions = get_png_dimensions(request.raw_body)
+    scaled_dimensions = scale_dimensions(dimensions, max_scale_dimension)
+    html = generate_template(png_file, scaled_dimensions)
+    S3.put_object(
+        Bucket=BUCKET,
+        Key=html_file,
+        Body=html,
+        ACL='public-read',
+        ContentDisposition='inline',
+        ContentType='text/html'
+    )
     return {
-        'png_url': "https://s3.amazonaws.com/{0}/{1}".format(BUCKET, png_file),
-        'html_url': "https://s3.amazonaws.com/{0}/{1}".format(BUCKET, html_file)
+        'png_url': get_png_url(png_file),
+        'html_url': get_html_url(html_file)
     }
 
+if __name__ == "__main__":
+    import urllib.request
+
+    response = urllib.request.urlopen('https://www.gstatic.com/webp/gallery3/3.png')
+    print(response)
+    dimensions = get_png_dimensions(response.read())
+    print(scale_dimensions(dimensions, 50))
+    print(scale_dimensions(dimensions, 500))
+    print(scale_dimensions(dimensions, 600))
+    print(scale_dimensions(dimensions, 800))
+    print(scale_dimensions(dimensions, 1000))
+    print(get_latitude_from_ip('0.0.0.0'))
+    print(get_latitude_from_ip('127.0.0.1'))
+    print(get_latitude_from_ip('255.255.255.255'))
+    print("Longitude: {0}".format(get_get_longitude_from_time()))
